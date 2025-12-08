1bebd74f46421c6ee4569b94359e7ac013b8af04
ole-hc <hofmann.ole.eibach@gmail.com>
2025-12-08 10:24:33 +0100

    Add money motor method

--- src/main/java/org/swtthm/bandit/CoinMotor.java
+++ src/main/java/org/swtthm/bandit/CoinMotor.java
@@ -1,6 +1,23 @@
 package org.swtthm.bandit;
 
+import ev3dev.actuators.lego.motors.EV3MediumRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.utility.Delay;
+
 public class CoinMotor {
+    final EV3MediumRegulatedMotor motor;
+
+    public CoinMotor() {
+        motor = new EV3MediumRegulatedMotor(MotorPort.A);
+        motor.setSpeed(250);
+        motor.brake();
+    }
+
     public void outputMoney() {
+        motor.forward();
+        motor.rotateTo(180);
+        Delay.msDelay(2500);
+        motor.backward();
+        motor.rotateTo(0);
     }
 }
