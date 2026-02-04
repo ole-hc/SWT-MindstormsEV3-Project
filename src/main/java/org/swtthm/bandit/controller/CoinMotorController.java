c66b747510843d17ef55ca56bca877ebebbecef9
ole <hofmann.ole.eibach@gmail.com>
2026-02-04 14:29:46 +0100

    Update CoinMotorController.java

--- src/main/java/org/swtthm/bandit/controller/CoinMotorController.java
+++ src/main/java/org/swtthm/bandit/controller/CoinMotorController.java
@@ -7,15 +7,19 @@ import lejos.utility.Delay;
 public class CoinMotorController {
     private final EV3MediumRegulatedMotor motor;
 
+    // Konstruktor legt den Motor an und konfiguriert ihn entsprechend
     public CoinMotorController() {
         motor = new EV3MediumRegulatedMotor(MotorPort.A);
         motor.setSpeed(250);
         motor.brake();
     }
 
+    // Funktion zum öffnen der Bank
     public void outputMoney() {
+        // Richtung setzen und öffnen
         motor.forward();
         motor.rotateTo(180);
+        // warten und in Ausgangsposition fahren
         Delay.msDelay(2500);
         motor.backward();
         motor.rotateTo(0);
